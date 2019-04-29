7c3f8b765f17d2383191f19bb6ba625b2007890c
jdamonew <41210780+jdamonew@users.noreply.github.com>
2019-04-29 14:45:16 -0300

    Commit_Teste

--- src/main/java/com/jobia/prova/ProvaApplication.java
+++ src/main/java/com/jobia/prova/ProvaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
+//teste
 @SpringBootApplication
 @ComponentScan(basePackages = "com.jobia.prova")
 public class ProvaApplication {
